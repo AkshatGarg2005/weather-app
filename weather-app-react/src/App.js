cfa592b562a475ea73fa19c43e7413b26740f20f
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2024-04-21 21:37:13 +0530

    1

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,16 @@
+import React , {useState} from "react";
+import axios from "axios"
+
+function App() {
+
+  // const url=`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=68d818c556c425ea2fa66bac593ce955`
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
